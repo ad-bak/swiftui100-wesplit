244436caf2aa9a8695d7a222e8a1c0c1636473a1
Adnan <adbak.dev@gmail.com>
2025-02-10 00:03:45 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adnan Bakaev  on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
